bdd7b3175ebb8ebf810553755dbf3ede089d8908
Federico Roux <federico.roux@globant.com>
2022-05-02 13:25:56 +0000

    encoder init

---
+++ src/encoder/encoder.hpp
@@ -0,0 +1,21 @@
+#include <ringbuffer.hpp>
+
+
+class encoder {
+
+public:
+
+    encoder() {}
+    ~encoder() {}
+
+    log_position()
+
+
+
+private:
+
+    ringbuffer<unsigned int, n_samples> pos;
+
+    static constexpr n_samples = 8;
+
+};
